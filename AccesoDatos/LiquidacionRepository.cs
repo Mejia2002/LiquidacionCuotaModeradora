58c2cbf51c05c4110863d975843639a8de37e5de
Mejia2002 <namejia@unicesar.edu.co>
2023-09-22 13:08:01 -0500

    Creacion de capas, referencias, creacion del codigo de la capa entidades y creacion de alguos metodos en la capa de Acceso a datos

---
+++ AccesoDatos/LiquidacionRepository.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Entidades;
+
+namespace AccesoDatos
+{
+    public class LiquidacionRepository
+    {
+        string Archivo = "LiquidacionCuotaModeradora.txt";
+        public string Guardar(Liquidacion liquidacion)
+        {
+            StreamWriter escritor = new StreamWriter(Archivo, true);
+            escritor.WriteLine(liquidacion.ToString());
+            escritor.Close();
+            return "Datos guardados";
+        }
+
+        public string Guardar(List<Liquidacion> liquidaciones) 
+        {
+            StreamWriter escritor = new StreamWriter(Archivo, false);
+            foreach (var item in liquidaciones)
+            {
+                escritor.WriteLine(item.ToString());
+            }
+            escritor.Close();
+            return $"Datos actualizados";
+        }
+
+        private Liquidacion Map(string linea)
+        {
+
+            Liquidacion liquidacion = new Liquidacion();
+            char delimiter = ';';
+            string[] matrizLiquidacion = linea.Split(delimiter);
+            establecimiento.IdentificacionEstablecimiento = (matrizLiquidacion[0]);
+            establecimiento.NombreEstablecimiento = matrizLiquidacion[1];
+            establecimiento.IngresosAnuales = Convert.ToDecimal(matrizLiquidacion[2]);
+            establecimiento.GastosAnuales = Convert.ToDecimal(matrizLiquidacion[3]);
+            establecimiento.TiempoFuncionamiento = int.Parse(matrizLiquidacion[4]);
+            establecimiento.TipoResposabilidad = Convert.ToChar(matrizLiquidacion[5]);
+            establecimiento.Ganancias = Convert.ToDecimal(matrizLiquidacion[6]);
+            establecimiento.ValorUvt = Convert.ToDecimal(matrizLiquidacion[7]);
+            establecimiento.Tarifa = Convert.ToDecimal(matrizLiquidacion[8]);
+            establecimiento.ValorImpuesto = Convert.ToDecimal(matrizLiquidacion[9]);
+            return establecimiento;
+
+        }
+
+        public void Eliminar(string numLiquidacion)
+        {
+            List<Liquidacion> establecimientos = new List<Liquidacion>();
+            liquidaciones = ConsultarTodos();
+            FileStream file = new FileStream(Archivo, FileMode.Create);
+            file.Close();
+            foreach (var item in establecimientos)
+            {
+                if (!EsEncontrado(item.NumLiquidacion, numLiquidacion))
+                {
+                    Guardar(item);
+                }
+
+            }
+
+        }
+    }
+}
--- Entidades/Liquidacion.cs
+++ Entidades/Liquidacion.cs
@@ -9,6 +9,7 @@ namespace Entidades
     public class Liquidacion
     {
         public string NumLiquidacion { get; set; }
+        public DateTime Fecha { get; set; }
         public string Identificacion { get; set; }
         public char TipoAfiliacion { get; set; }
         public decimal Salario { get; set; }
@@ -21,19 +22,21 @@ namespace Entidades
         {
         }
 
-        public Liquidacion(string numLiquidacion, string identificacion, char tipoAfiliacion, decimal salario, decimal valorServivio, decimal tarifa, decimal cuotaModeradora)
+        public Liquidacion(string numLiquidacion, DateTime fecha, string identificacion, char tipoAfiliacion, decimal salario, decimal valorServivio, decimal tarifa, decimal cuotaModeradora)
         {
             NumLiquidacion = numLiquidacion;
+            Fecha = fecha;
             Identificacion = identificacion;
             TipoAfiliacion = tipoAfiliacion;
             Salario = salario;
             ValorServivio = valorServivio;
             Tarifa = tarifa;
             CuotaModeradora = cuotaModeradora;
+            
         }
         public override string ToString()
         {
-            return $"{NumLiquidacion};{Identificacion};{TipoAfiliacion};{Salario};{ValorServivio};{Tarifa};{CuotaModeradora}";
+            return $"{NumLiquidacion};{Fecha};{Identificacion};{TipoAfiliacion};{Salario};{ValorServivio};{Tarifa};{CuotaModeradora}";
         }
 
     }
